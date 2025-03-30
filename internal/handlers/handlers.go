902f19c255ef2b0a51834dceaff5defcf1d814f2
Ilnur.Fatkullin <Ilnur.Fatkullin@x5.ru>
2025-03-30 08:53:15 +0300

    промежуточный результат

---
+++ internal/handlers/handlers.go
@@ -0,0 +1,5 @@
+package handlers
+
+func Add(a, b int) int {
+	return a + b
+}
---
+++ internal/repositories/repositories.go
@@ -0,0 +1,6 @@
+package repositories
+
+// Repository - интерфейс для работы с данными
+type Repository interface {
+	GetData() string
+}
