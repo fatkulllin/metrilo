efaf07d268ce2a6e251723b5caa20ab5d343c779
Ilnur.Fatkullin <Ilnur.Fatkullin@x5.ru>
2025-04-04 11:22:20 +0500

    test

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -84,10 +84,10 @@ func GetMetric(res http.ResponseWriter, req *http.Request) {
 		http.Error(res, "Only GET requests are allowed!!", http.StatusMethodNotAllowed)
 		return
 	}
-	if req.Header.Get("Content-Type") != "text/plain" {
-		http.Error(res, "Only Content-Type: text/plain header are allowed!!", http.StatusMethodNotAllowed)
-		return
-	}
+	// if req.Header.Get("Content-Type") != "text/plain" {
+	// 	http.Error(res, "Only Content-Type: text/plain header are allowed!!", http.StatusMethodNotAllowed)
+	// 	return
+	// }
 	typeMetric := chi.URLParam(req, "type")
 	nameMetric := chi.URLParam(req, "name")
 	// if !isLetter(nameMetric) {
