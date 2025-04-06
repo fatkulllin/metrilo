75c7563d7f0d127be9dea87da25b088434c6a493
Ilnur.Fatkullin <Ilnur.Fatkullin@x5.ru>
2025-04-06 22:04:14 +0500

    test

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"github.com/fatkulllin/metrilo/internal/server"
-	"github.com/spf13/pflag"
 )
 
 func main() {
-	var address *string = pflag.StringP("address", "a", "localhost:8080", "port start server")
-	pflag.Parse()
-	server := server.NewServer(address)
+
+	server := server.NewServer()
 	server.Start()
 
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,21 +1,56 @@
 package server
 
 import (
+	"errors"
 	"fmt"
+	"log"
+	"net"
 	"net/http"
+	"strconv"
 
 	"github.com/fatkulllin/metrilo/internal/handlers"
 	"github.com/go-chi/chi"
+	"github.com/spf13/pflag"
 )
 
-type Server struct {
-	Address *string
+type NetAddress struct {
+	Host string
+	Port int
 }
 
-func NewServer(address *string) *Server {
+func (a *NetAddress) Type() string {
+	return "netAddress"
+}
+
+func (a NetAddress) String() string {
+	return a.Host + ":" + strconv.Itoa(a.Port)
+}
+
+func (a *NetAddress) Set(s string) error {
+	host, portStr, err := net.SplitHostPort(s)
+	if err != nil {
+		return errors.New("need address in the form host:port")
+	}
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		return err
+	}
+	a.Host = host
+	a.Port = port
+	return nil
+}
+
+type Server struct {
+	Address NetAddress
+}
+
+func NewServer() *Server {
 	fmt.Println("Initializing server...")
 	return &Server{
-		Address: address,
+		Address: NetAddress{
+			Host: "localhost",
+			Port: 8080,
+		},
 	}
 }
 
@@ -28,6 +63,12 @@ func (server *Server) Router() *chi.Mux {
 }
 
 func (server *Server) Start() {
-	fmt.Printf("Start server on %s...", *server.Address)
-	http.ListenAndServe(*server.Address, server.Router())
+	pflag.VarP(&server.Address, "address", "a", "localhost:8080")
+	pflag.Parse()
+	bindAddress := server.Address.String()
+	log.Printf("Server started on %s...", bindAddress)
+	err := http.ListenAndServe(bindAddress, server.Router())
+	if err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
 }
